173e7f0cec1d094625cc868f6dbf59140e0385ae
Chang Gwi Lee <lcg5450@afreecatv.com>
2017-01-09 12:17:42 +0900

    기본파일 다시 커밋.

---
+++ GithubIssues/Cells/IssueCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  IssueCollectionViewCell.swift
+//  GithubIssues
+//
+//  Created by Chang Gwi Lee on 2017. 1. 6..
+//  Copyright © 2017년 AfreecaTV. All rights reserved.
+//
+
+import UIKit
+
+class IssueCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var issueLabel: UILabel!
+}
